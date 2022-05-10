6ccdf39ccc327005c0fe3a9eded2057507874e7b
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-05-11 02:30:09 +0600

    initial commit with console write

---
+++ c_sharp_tutorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace c_sharp_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.print();
+        }
+
+        void print()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
