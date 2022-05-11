e8196bb8825e1a3332b1e22ccf62d7da10b55a5e
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-05-11 18:38:27 +0600

    input with commandline command dontnet run arguments

--- c_sharp_tutorial/Program.cs
+++ c_sharp_tutorial/Program.cs
@@ -6,6 +6,7 @@ namespace c_sharp_tutorial
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(args[0]);
             Program program = new Program();
             program.print();
         }
