e143272d2e43ff57f9c48d458cb35bb9bcde196b
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-05-13 05:11:48 +0600

    decimal method

--- c_sharp_tutorial/Program.cs
+++ c_sharp_tutorial/Program.cs
@@ -6,29 +6,8 @@ namespace c_sharp_tutorial
     {
         static void Main(string[] args)
         {
-            //value types
-            int a = -5;
-            int b = a;
-
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-
-            b = 10;
-            // only b will change
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-
-            //reference type
-            int[] c = { 5 };
-            int[] d = c;
-
-            Console.WriteLine(c[0]);
-            Console.WriteLine(d[0]);
-
-            d[0] = 10;
-
-            Console.WriteLine(c[0]);
-            Console.WriteLine(d[0]);
+            double a = Math.Pow(9999, 999999999);
+            Console.WriteLine(double.IsInfinity(a));
         }
 
     }
