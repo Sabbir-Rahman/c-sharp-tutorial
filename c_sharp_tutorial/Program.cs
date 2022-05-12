d32b4b5a20b75b63186a0b19a696e86962277e91
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-05-13 05:19:57 +0600

    char types

--- c_sharp_tutorial/Program.cs
+++ c_sharp_tutorial/Program.cs
@@ -5,10 +5,13 @@ namespace c_sharp_tutorial
     class Program
     {
         static void Main(string[] args)
-        {
-            double x = 50.9;
-            double y = 300;
-            Console.WriteLine(Math.Floor(x));
+        {   
+            // single quote to char
+            char letter = 'a';
+            // double quote to string
+
+            // char and integer are interchangable
+            int number = 'a';
         }
 
     }
